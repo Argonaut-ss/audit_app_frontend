ef78ae4e305c620ec3e9f7969f7c0762590ae5ae
sam <81003477+Argonaut-ss@users.noreply.github.com>
2026-08-10 14:52:27 +0700

    Feat : dosen page and API integrated

---
+++ src/services/admin/dosen/dosen.js
@@ -0,0 +1,26 @@
+import api from "../../api";
+
+export const importDosen = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const response = await api.post(
+    "/api/dosens/import",
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+
+  return response.data;
+};
+
+export const getDosen = async (params = {}) => {
+    const response = await api.get("/api/dosens", {
+      params,
+    });
+  
+    return response.data;
+  };
